9a32e52e3b0e7bc0bfd75dcafd8f8bc3b320478b
JacksonRCode <jackdareid@gmail.com>
2025-11-19 15:29:48 -0500

    Fix file structure and initialize database.

--- backend/config.py
+++ backend/config.py
@@ -8,15 +8,16 @@ class Settings:
     PROJECT_NAME: str = "Clubr"
     VERSION: str = "1.0.0"
     
-    # We use the POOLED connection for the actual app
-    # This prevents "Too many connections" errors on Vercel
-    DATABASE_URL: str = os.getenv("postgresql://neondb_owner:******@ep-raspy-cloud-a8oe00xu-pooler.eastus2.azure.neon.tech/clubr?sslmode=require")
+    # CORRECT: Ask for the variable named "DATABASE_URL"
+    DATABASE_URL: str = os.getenv("DATABASE_URL")
     
-    # We keep the SECRET_KEY here too
-    JWT_SECRET_KEY: str = os.getenv("5d49140a5fdc4509452b0ac0e0d5214408262b657dae7af6f1f887d89570249b")
+    # CORRECT: Ask for the variable named "JWT_SECRET_KEY"
+    JWT_SECRET_KEY: str = os.getenv("JWT_SECRET_KEY")
 
 # Validate that the DB URL exists
+# If you used your previous code, this would have raised the error because 
+# os.getenv() would return None.
 if not Settings.DATABASE_URL:
-    raise ValueError("DATABASE_URL is missing from .env file")
+    raise ValueError("DATABASE_URL not working!!")
 
 settings = Settings()
