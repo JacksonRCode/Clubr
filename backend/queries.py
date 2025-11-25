a69cb3515225433c36f07cd7fcfdd36226af7a23
mystic-bee <129250068+mystic-bee@users.noreply.github.com>
2025-11-25 01:29:08 -0500

    Create db functions for membership (follower + admin)

--- backend/queries.py
+++ backend/queries.py
@@ -242,7 +242,7 @@ def get_recommended_clubs(db: Session, user_id: int):
         if not user_tags:
             return []
         
-        # Convert into set of tag ids
+        # Convert into set of user tag ids
         user_tag_ids = set()
         for tag in user_tags:
             user_tag_ids.add(tag.tagid)
@@ -277,4 +277,188 @@ def get_recommended_clubs(db: Session, user_id: int):
         print(f"Error has occured when trying to get recommended clubs for user {user_id}: {e}")
         return []
 
+# ------------ DB Functions for Membership ------------
 
+# Database function for checking membership
+def check_membership(db: Session, user_id: int, club_id: int):
+    try:
+        # Check if in ClubMembership table
+        membership = db.query(ClubMembership).filter_by(userid=user_id, clubid=club_id).first()
+        return membership
+    
+    except Exception as e:
+        db.rollback()
+        print(f"Error when checking membership for user {user_id} and club {club_id}: {e}")
+        return None
+
+# Database function for following a club
+def follow_club(db: Session, user_id: int, club_id: int):
+
+    # Check if already following
+    existing_follower = check_membership(db, user_id, club_id)
+
+    if existing_follower:
+        return None
+    
+    # Create ClubMembership Object
+    new_follower = ClubMembership(userid=user_id, clubid=club_id, role="Follower")
+
+    # Save to DB
+    try:
+        db.add(new_follower)
+        db.commit()
+        db.refresh(new_follower)
+        return new_follower
+
+    except Exception as e:
+        db.rollback()
+        print(f"Error when adding new follower: {e}")
+        return None
+
+# Database function for unfollowing a club
+def unfollow_club(db: Session, user_id: int, club_id: int):
+
+    # Check if already following
+    existing_follower = check_membership(db, user_id, club_id)
+
+    # If follower doesn't exist, return None
+    if existing_follower is None:
+        return None
+
+    try:
+        # Remove follower
+        db.query(ClubMembership).filter_by(userid=user_id, clubid=club_id).delete()
+        db.commit()
+
+    # Rollback and print error mssg in case of error
+    except Exception as e:
+        db.rollback()
+        print(f"Error has occured when unfollowing: {e}")
+        return None
+
+# Database function for getting all followers for a specific user
+def get_all_followed_clubs(db: Session, user_id: int):
+    try:
+        # Get all clubs that the user follows
+        all_followed_clubs = db.query(Clubs).join(ClubMembership, Clubs.clubid == ClubMembership.clubid).filter(ClubMembership.userid == user_id).all()
+        return all_followed_clubs
+
+    except Exception as e:
+        # Rollback in case of error
+        db.rollback()
+        print(f"Error has occured when getting all followed clubs for user {user_id}: {e}")
+        return None
+
+# Database function for getting all followers for a specific club
+def get_all_club_followers(db: Session, club_id: int):
+    try:
+        # Get all followers for that club
+        all_followers = db.query(Users).join(ClubMembership, Users.userid == ClubMembership.userid).filter(ClubMembership.clubid == club_id).all()
+        return all_followers
+    
+    except Exception as e:
+        # Rollback in case of error
+        db.rollback()
+        print(f"Error has occured when getting all followers for club {club_id}: {e}")
+        return None
+
+# Database function to check admin status for a specific user and club
+def check_admin_status(db: Session, user_id: int, club_id: int):
+    try:
+        # Check club membership table
+        membership = check_membership(db, user_id, club_id)
+
+        # Check if membership found and if role is Admin
+        if membership is not None and membership.role=="Admin":
+            print(f"User {user_id} is a club admin for club {club_id}")
+            return True
+        else:
+            print(f"User {user_id} is not a club admin for club {club_id}")
+            return False
+    
+    except Exception as e:
+        # Rollback in case of error
+        db.rollback()
+        print(f"Error has occured when checking admin status for user {user_id} in {club_id}: {e}")
+        return False
+
+# Database function to get all club admins for a specific club
+def get_club_admins(db: Session, club_id: int):
+    try: 
+        # Get all club admins for a specific club
+        club_admins = db.query(Users).join(ClubMembership).filter(ClubMembership.clubid == club_id, ClubMembership.role=="Admin").all()
+        return club_admins
+    
+    except Exception as e:
+        # Rollback in case of error
+        db.rollback()
+        print(f"Error has occured when getting all club admins for club {club_id}: {e}")
+        return None
+
+# Database function to get all clubs for which the user is an admin
+def get_all_admin_clubs_for_user(db: Session, user_id: int):
+    try:
+        # Get all clubs that the user is an admin for
+        all_admin_clubs = db.query(ClubMembership).filter_by(userid=user_id, role="Admin").all()
+        return all_admin_clubs
+
+    except Exception as e:
+        # Rollback in case of error
+        db.rollback()
+        print(f"Error has occured when getting all clubs for which the user {user_id} is an admin: {e}")
+        return None
+
+# Database function to add new club admin
+def add_club_admin(db: Session, user_id: int, club_id: int):
+    try:
+        # Check club membership table
+        membership = check_membership(db, user_id, club_id)
+
+        # If membership does not exist
+        if membership is None:
+
+            # Create new ClubMembership object with Admin role
+            new_admin = ClubMembership(userid=user_id, clubid=club_id, role="Admin")
+
+            # Save to DB
+            db.add(new_admin)
+            db.commit()
+            return new_admin
+        
+        # If membership exists
+        else:
+            # If role is not Admin, change to Admin
+            if membership.role!="Admin":
+                membership.role="Admin"
+                db.commit()
+            return membership
+
+    except Exception as e:
+        # Rollback in case of error
+        db.rollback()
+        print(f"Error has occured when adding new club admin {user_id} for club {club_id}: {e}")
+        return None
+
+# Database function to remove a club admin
+def remove_club_admin(db: Session, user_id: int, club_id: int):
+    try:
+        # Check club membership table
+        membership = check_membership(db, user_id, club_id)
+
+        # If membership does not exist
+        if membership is None:
+            return None
+        
+        # If membership role is Admin
+        if membership.role=="Admin":
+
+            # Update role from Admin to Follower
+            db.query(ClubMembership).filter_by(userid=user_id, clubid=club_id).update({"role": "Follower"})
+            db.commit()
+    
+    # Rollback and print error mssg in case of error
+    except Exception as e:
+        db.rollback()
+        print(f"Error has occured when removing club admin {user_id} from club {club_id}: {e}")
+        return None
+    
